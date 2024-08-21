e9adac97ef04ae599ca6978f5e0644b1d0093bef
Your Name <you@example.com>
2024-08-21 18:42:59 +0000

    h

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,21 @@
-var http = require('http');
-const port = 1234;
-const host = 'localhost';
-const app = http.creatServer((req,res)=>{
-	res.statusCode=200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello Holberton School!\n');
-}
-app.listen(port,host,()=>{
-	console.log('Server running at http://${HOST}:${port}/`);
+const http = require('http');
+
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
